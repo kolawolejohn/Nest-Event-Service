f0d3a5d2b127812f159acd6acf437d4cb6d63ec5
kolawole ogunfowokan <kolawole.ogunfowokan@edukoya.com>
2022-07-17 22:43:27 +0100

    dockerize-and-add-db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Events } from './events/entity/events.entity';
+import { EventsController } from './events/events.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    username: 'root',
+    password: 'example',
+    database: 'nest-events',
+    entities: [Events],
+    synchronize: true
+  }),
+   TypeOrmModule.forFeature([Events])
+],
+  controllers: [AppController, EventsController],
   providers: [AppService],
 })
 export class AppModule {}
